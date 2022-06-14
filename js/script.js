7fb8c2c082cee2a5abaa5c98780fc8ee36eae6e0
oleg <oleg.tolmachev89@gmail.com>
2022-06-14 16:38:55 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -10,12 +10,38 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+} else { 
+    console.log ('err');
+    i--;}
+}
+
+if (personalMovieDB.count < 10) {
+    alert ('Просмотрено довольно мало фильмов');
+} else if (10 <= personalMovieDB.count < 30) {
+    alert ('Вы классический зритель')
+} else if (personalMovieDB.count >= 30) {
+    alert ('Вы киноман!');
+} else {
+    alert ('Error')
+}
+
+
+
+
+
+// // const a = prompt('Один из последних просмотренных фильмов?', ''),
+// //       b = prompt('На сколько оцените его?', ''),
+// //       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
